3cbf6af1f06775f96cdbaab0a9fc0d4f4da49a18
the-codeholic <shanofficial2000@gmail.com>
2022-02-07 21:53:27 +0530

    udemy

---
+++ Udemy/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Shaheem PP on 19/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
